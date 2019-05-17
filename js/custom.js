a36875fa0b6c9c11ed6af76a0af6cf3ad2a48d86
Marisa Ramon <mer0522@gmail.com>
2019-05-17 09:15:03 -0500

    complete services section

---
+++ js/custom.js
@@ -0,0 +1,11 @@
+// Services 
+
+// used to make a function available once page has loaded
+
+// short form: $(function() {  });
+
+
+$(document).ready(function() {
+// animate on scroll
+    new WOW().init();
+});
